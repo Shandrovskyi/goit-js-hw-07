7686f4dfa2467681721546646733dbf4cd11d872
Shandrovskyi <119674969+Shandrovskyi@users.noreply.github.com>
2023-04-26 21:19:21 +0300

    Create 01-gallery.js

---
+++ js/01-gallery.js
@@ -0,0 +1,24 @@
+
+import { galleryItems } from './gallery-items.js';
+console.log(galleryItems);
+
+
+const galleryUL = document.querySelector(".gallery");
+
+
+
+const document.createElement = () =>
+
+<li class="gallery__item">
+<a class="gallery__link" href="large-image.jpg">
+  <img
+    class="gallery__image"
+    src="small-image.jpg"
+    data-source="large-image.jpg"
+    alt="Image description"
+  />
+</a>
+</li>;
+
+
+
